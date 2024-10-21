c0121afd1cebf4566e093ff1812be866dc5ef992
Maju <mjpo@cesar.school>
2024-10-21 11:11:57 -0300

    add rascunho de Programa

--- projetoPOO/src/br/com/cesarschool/poo/titulos/Programa.java
+++ projetoPOO/src/br/com/cesarschool/poo/titulos/Programa.java
@@ -1,5 +1,20 @@
 package br.com.cesarschool.poo.titulos;
 
-public class Programa {
+import java.util.Scanner;
 
+import br.com.cesarschool.poo.titulos.mediators.MediatorAcao;
+import br.com.cesarschool.poo.titulos.mediators.MediatorEntidadeOperadora;
+import br.com.cesarschool.poo.titulos.mediators.MediatorOperacao;
+import br.com.cesarschool.poo.titulos.mediators.MediatorTituloDivida;
+
+public class Programa {
+	static Scanner ENTRADA = new Scanner(System.in);
+	static MediatorAcao mediatorAcao = MediatorAcao.getInstance();
+	static MediatorTituloDivida mediatorTituloDivida = MediatorTituloDivida.getInstance();
+	static MediatorEntidadeOperadora mediatorEntidadeOperadora = MediatorEntidadeOperadora.getInstance();
+	static MediatorOperacao mediatorOperacao = MediatorOperacao.getInstance();
+
+	public static void main(String[] args) {
+		
+	}
 }
